bd7a3acfa43a7814eb93f952a0285663bde60574
Juan Balza <balza732@gmail.com>
2023-11-07 21:35:22 -0500

    Se trabaja en la funcionalidad de la carta del producto y Ourshop

--- src/pages/ourShop/OurShop.jsx
+++ src/pages/ourShop/OurShop.jsx
@@ -1,8 +1,35 @@
+import "./OurShop.css";
 import Card from "../../componets/card/Card";
 import img from "./../../assets/img/img-proteinas/bipro-3lb-nueva-presentacion.webp"
 const OurShop = () => {
+    const products = [
+        {
+            id: 1,
+            title: "Creatina",
+            description: "Te ayuda a la recuperacion muscular",
+            img: "http://placekitten.com/200/300"
+        },
+        {
+            id: 2,
+            title: "Creatina1",
+            description: "Te ayuda a la recuperacion muscular",
+            img: "http://placekitten.com/200/300"
+        },
+        {
+            id: 3,
+            title: "Creatina2",
+            description: "Te ayuda a la recuperacion muscular",
+            img: "http://placekitten.com/200/300"
+        },
+    ];
     return (
-        <Card img={img} />
+        <div className="card-container"> 
+            {
+                products.map((product, index) => <Card img={product.img} description={product.description} title={product.title} key={index} /> )
+            }
+        </div>
+
+        // <Card img={img} />
     );
 }
 export default OurShop;
