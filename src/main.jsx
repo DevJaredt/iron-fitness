b77c30ab88134dc47ecf1f92f194c9cb2f0ed2d9
Cesar Bermudez <cesar.bermudez.sierra@gmail.com>
2023-11-14 23:23:05 -0500

    Se ajusta workflows

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,6 @@ import './index.css'
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <App />
