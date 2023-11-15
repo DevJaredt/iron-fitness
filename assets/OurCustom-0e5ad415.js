b1dd9cdbeecce2ca5d45f2cc910584cb9359e6de
Cbermudez98 <54110212+Cbermudez98@users.noreply.github.com>
2023-11-15 05:04:27 +0000

    Deploying to gh-pages from @ DevJaredt/iron-fitness@9a45df6b7a3f931df344e9b626582907ae23258d 🚀

--- assets/OurCustom-0e5ad415.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-892e6abf.js";const s=()=>r.jsx("h1",{children:"Our custom"});export{s as default};
---
+++ assets/OurCustom-6f75d928.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-c89f5e3c.js";const s=()=>r.jsx("h1",{children:"Our custom"});export{s as default};
