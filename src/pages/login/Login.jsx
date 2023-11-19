4aeb3c350e4a67e6c8f88b785d70a858e842ddd3
Jared Trujillo <jaredtrujilloespinosa64@gmail.com>
2023-11-19 14:26:21 -0500

    se ajusto el login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -34,29 +34,61 @@ const Login = () => {
 
     return (
     <section className="main-bg">
-        <div className="login-container text-c animated flipInX">
-                <div>
-                    <h1 className="logo-badge text-whitesmoke"><span className="fa fa-user-circle"></span></h1>
-                </div>
-                    <h3 className="text-whitesmoke">Sign In Template</h3>
-                    <p className="text-whitesmoke">Sign In</p>
-                <div className="container-content">
-                    <form onSubmit={(e) => onSubmitHandler(e)} className="margin-t">
-                        <div className="form-group">
-                            <input onKeyUp={(e) => setEmail(e.target.value)} type="text" className="form-control" placeholder="Username" required=""/>
+                <div className="container" id="container">
+                <div className="form-container sign-up-container">
+                    <form action="#">
+                        <h1>Create Account</h1>
+                        <div className="social-container">
+                            <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
+                            <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
+                            <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
                         </div>
-                        <div className="form-group">
-                            <input onKeyUp={(e) => setPassword(e.target.value)} type="password" className="form-control" placeholder="*****" required=""/>
-                        </div>
-                        <input type="submit" className="form-button button-l margin-b" value={"Sing in"}/> 
-        
-                        <a className="text-darkyellow" href="#"><small>Forgot your password?</small></a>
-                        <p className="text-whitesmoke text-center"><small>Do not have an account?</small></p>
-                        <a className="text-darkyellow" href={"/register"}><small>Sign Up</small></a>
+                        <span>or use your email for registration</span>
+                        <input type="text" placeholder="Name" />
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <button>Sign Up</button>
                     </form>
-                    <p className="margin-t text-whitesmoke"><small> IronFitness &copy; 2023</small> </p>
+                </div>
+                <div className="form-container sign-in-container">
+                    <form action="#">
+                        <h1>Sign in</h1>
+                        <div className="social-container">
+                            <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
+                            <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
+                            <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
+                        </div>
+                        <span>or use your account</span>
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <a href="#">Forgot your password?</a>
+                        <button>Sign In</button>
+                    </form>
+                </div>
+                <div className="overlay-container">
+                    <div className="overlay">
+                        <div className="overlay-panel overlay-left">
+                            <h1>Welcome Back!</h1>
+                            <p>To keep connected with us please login with your personal info</p>
+                            <button className="ghost" id="signIn">Sign In</button>
+                        </div>
+                        <div className="overlay-panel overlay-right">
+                            <h1>Hello, Friend!</h1>
+                            <p>Enter your personal details and start journey with us</p>
+                            <button className="ghost" id="signUp">Sign Up</button>
+                        </div>
+                    </div>
                 </div>
             </div>
+
+            <footer>
+                <p>
+                    Created with <i className="fa fa-heart"></i> by
+                    <a target="_blank" href="https://florin-pop.com">Florin Pop</a>
+                    - Read how I created this and how you can join the challenge
+                    <a target="_blank" href="https://www.florin-pop.com/blog/2019/03/double-slider-sign-in-up-form/">here</a>.
+                </p>
+            </footer>
     </section> 
     );
 };
